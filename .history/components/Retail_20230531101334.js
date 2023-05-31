6ee3bd06612e56226979e0d94252eaab2d9f5b55
Diyorbek <idevdiyor@gmail.com>
2023-05-31 10:16:42 +0500

    Removed react-reveal

--- .history/components/Retail_20230531101334.js
+++ .history/components/Retail_20230531101334.js
@@ -1,79 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import CheckCircleOutlinedIcon from "@mui/icons-material/CheckCircleOutlined";
-
-function Wholesale() {
-  //
-
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    var myHeaders = new Headers();
-    myHeaders.append(
-      "Authorization",
-      "Basic d29ybGRtaXJhbEBtYWlsLnJ1OmMzSzUzNjdrMElLSDFydWNqNUMyNjQyMDU2RlA3dUJR"
-    );
-
-    var requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    };
-
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((result) => console.log(result))
-      .catch((error) => console.log("error", error));
-  }, []);
-
-  //
-
-  //
-  //
+import Fade from "react-reveal";
+function Retail() {
   return (
-    <div className="Wholesale">
+    <div className="Retail">
       <div className="containerdev">
         <div className="intro">
-          <h1>Оптовый маркетплейс</h1>
-          <p>20 000 + разновидностей товаров для оптовой поставки</p>
+          <h1>Розничный маркетплейс</h1>
+          <p>500 000 + товаровов для вас и вашего дома, офиса</p>
         </div>
         <div className="retail_products">
           <div className="retail_prod_wrapper">
-            {products.map((products) => {
-              <div className="product_wrap" key={products.products_id}>
-                <a href="">
-                  <div className="prod_image">
-                    <img src="https://all4u.market/images/thumbnails/550/450/detailed/26/44_ije7-ds.jpg.jpg" />
-                  </div>
-                  <div className="prod_status">
-                    <div className="top_wrap">
-                      <p>
-                        <CheckCircleOutlinedIcon /> В наличии
-                      </p>
-                      <p>Узбекистан</p>
-                    </div>
-                  </div>
-                  <div className="prod_price">
-                    <div>
-                      <h2>
-                        {products.products_id} <span>UZS</span>
-                      </h2>
-                      <h4>
-                        {" "}
-                        <s>13 111 111</s>
-                        <span>UZS</span>
-                      </h4>
-                    </div>
-                    <div className="discount">2%</div>
-                  </div>
-                  <div className="prod_about">
-                    <div>
-                      <h4>Samsung S22 Ultra 256GB </h4>
-                      <p>Bsxzxbzsxmzbsnxm bzmxzsnbxm zbsAXAXAXXA cawawcx </p>
-                    </div>
-                  </div>
-                </a>
-              </div>;
-            })}
-
             <div className="product_wrap">
               <a href="">
                 <div className="prod_image">
@@ -102,7 +39,7 @@ function Wholesale() {
                 </div>
                 <div className="prod_about">
                   <div>
-                    <h4>Samsung S22 Ultra 256GB </h4>
+                    <h4>Samsung S22 Ultra 256GB Black </h4>
                     <p>Bsxzxbzsxmzbsnxm bzmxzsnbxm zbsAXAXAXXA cawawcx </p>
                   </div>
                 </div>
@@ -137,7 +74,7 @@ function Wholesale() {
                 </div>
                 <div className="prod_about">
                   <div>
-                    <h4>Samsung S22 Ultra 256GB </h4>
+                    <h4>Samsung S22 Ultra 256GB Black </h4>
                     <p>Bsxzxbzsxmzbsnxm bzmxzsnbxm zbsAXAXAXXA cawawcx </p>
                   </div>
                 </div>
@@ -172,7 +109,42 @@ function Wholesale() {
                 </div>
                 <div className="prod_about">
                   <div>
-                    <h4>Samsung S22 Ultra 256GB </h4>
+                    <h4>Samsung S22 Ultra 256GB Black </h4>
+                    <p>Bsxzxbzsxmzbsnxm bzmxzsnbxm zbsAXAXAXXA cawawcx </p>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <div className="product_wrap">
+              <a href="">
+                <div className="prod_image">
+                  <img src="https://all4u.market/images/thumbnails/550/450/detailed/26/44_ije7-ds.jpg.jpg" />
+                </div>
+                <div className="prod_status">
+                  <div className="top_wrap">
+                    <p>
+                      <CheckCircleOutlinedIcon /> В наличии
+                    </p>
+                    <p>Узбекистан</p>
+                  </div>
+                </div>
+                <div className="prod_price">
+                  <div>
+                    <h2>
+                      13 111 111 <span>UZS</span>
+                    </h2>
+                    <h4>
+                      {" "}
+                      <s>13 111 111</s>
+                      <span>UZS</span>
+                    </h4>
+                  </div>
+                  <div className="discount">2%</div>
+                </div>
+                <div className="prod_about">
+                  <div>
+                    <h4>Samsung S22 Ultra 256GB Black </h4>
                     <p>Bsxzxbzsxmzbsnxm bzmxzsnbxm zbsAXAXAXXA cawawcx </p>
                   </div>
                 </div>
@@ -180,8 +152,10 @@ function Wholesale() {
             </div>
           </div>
           <a href="" className="click_retail product_wrap">
-            <div>
-              <h2> See Retail marketplace</h2>
+            <div className="for_back_wholesale">
+              <div className="content__this">
+                <h2>Посмотреть оптовую площадку</h2>
+              </div>
             </div>
           </a>
         </div>
@@ -190,4 +164,4 @@ function Wholesale() {
   );
 }
 
-export default Wholesale;
+export default Retail;
