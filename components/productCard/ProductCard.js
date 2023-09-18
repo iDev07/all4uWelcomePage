55a2cf6f655b0bf71521bdd082cd00fa60c62e1b
Diyorbek <idevdiyor@gmail.com>
2023-09-19 00:09:13 +0500

    Test

---
+++ components/productCard/ProductCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+const usd = 12157;
+export const ProductCard = ({ product }) => {
+  return (
+    <div className="product_wrap" key={product.id}>
+      {/* <a href=""> */}
+      <div className="prod_image">
+        <img src={product.main_pair.detailed.image_path} />
+      </div>
+      <div className="prod_status">
+        <div className="top_wrap">
+          <p>
+            <CheckCircleOutlinedIcon /> В наличии
+          </p>
+          <p>Узбекистан</p>
+        </div>
+      </div>
+      <div className="prod_price">
+        <div>
+          <h2>
+            {product.price * usd} <span>UZS</span>
+          </h2>
+          <h4>
+            {" "}
+            <span>1</span>
+            <span>UZS</span>
+          </h4>
+        </div>
+        <div className="discount">2%</div>
+      </div>
+      <div className="prod_about">
+        <div>
+          <h4>{product.product}</h4>
+          <p></p>
+        </div>
+      </div>
+      {/* </a> */}
+    </div>
+  );
+};
